c49e1dcc1e8877fa382d8cf29cfadeff4609d7e1
ppedvLukasK <lukask@ppedv.de>
2023-12-05 17:00:08 +0100

    Delete WpfApp1

--- TopLevelStatements/Program.cs
+++ TopLevelStatements/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine(args);
+﻿using System.Collections.Concurrent;
+
+Console.WriteLine(args);
+
+public class ConcurrentHashSet<T>
+{
+	private readonly ConcurrentDictionary<T, object> dict = new();
+
+	public void Add(T obj)
+	{
+		dict.TryAdd(obj, null);
+	}
+
+	public bool Contains(T obj)
+	{
+		return dict.ContainsKey(obj);
+	}
+}
